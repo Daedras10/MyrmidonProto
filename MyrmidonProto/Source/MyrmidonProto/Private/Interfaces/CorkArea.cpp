d9629523e867bbcce1dbff3bc62589c4a854d332
Arthur <35684550+Daedras10@users.noreply.github.com>
2024-07-04 13:37:42 +0200

    C++ interfaces

---
+++ MyrmidonProto/Source/MyrmidonProto/Private/Interfaces/CorkArea.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/CorkArea.h"
+
+// Add default functionality here for any ICorkArea functions that are not pure virtual.
