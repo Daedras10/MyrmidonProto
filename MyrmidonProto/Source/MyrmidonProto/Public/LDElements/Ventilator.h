c922c653c975fc21c3217e5af3a21cc0bd1e8cc5
Arthur <35684550+Daedras10@users.noreply.github.com>
2024-07-09 12:01:54 +0200

    Ventilators activated by buttons

---
+++ MyrmidonProto/Source/MyrmidonProto/Public/LDElements/Ventilator.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/VisualSwitcher.h"
+#include "Ventilator.generated.h"
+
+UCLASS()
+class MYRMIDONPROTO_API AVentilator : public AActor, public IVisualSwitcher
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AVentilator();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float WindPower = 1.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector WindDirection = FVector(0.0f, 0.0f, 0.0f);
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector WindBoxScale = FVector(1.0f, 1.0f, 1.0f);
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float VentilatorHeightMult = 1.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool Activated = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float RotationSpeed = 1.0f;
+
+};
