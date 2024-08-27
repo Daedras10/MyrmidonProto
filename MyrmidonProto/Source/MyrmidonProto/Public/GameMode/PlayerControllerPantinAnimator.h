8193136643c9b4f36ba69aae44736a505fb31b89
Arthur <35684550+Daedras10@users.noreply.github.com>
2024-08-27 11:39:29 +0200

    New bridge adjustment for network delay

--- MyrmidonProto/Source/MyrmidonProto/Public/GameMode/PlayerControllerPantinAnimator.h
+++ MyrmidonProto/Source/MyrmidonProto/Public/GameMode/PlayerControllerPantinAnimator.h
@@ -86,7 +86,7 @@ public:
 	float CircleStartAngle = -1.0f;
 
 	UPROPERTY(BlueprintReadWrite)
-	float ProgressionAllowed = 0.05f;
+	float ProgressionAllowed = 0.15f;
 
 	UPROPERTY(BlueprintReadWrite)
 	float CurrentProgressionPositive = 0.0f;
