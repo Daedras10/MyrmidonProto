cb0d9ac47bcff281a36100418b35a1880bdbae78
Arthur <daedras10@gmail.com>
2024-09-06 17:02:27 +0200

    New distance with angle

--- MyrmidonProto/Source/MyrmidonProto/Private/FunctionLibrary/MyrmidonBlueprintFunctionLibrary.cpp
+++ MyrmidonProto/Source/MyrmidonProto/Private/FunctionLibrary/MyrmidonBlueprintFunctionLibrary.cpp
@@ -3,3 +3,15 @@
 
 #include "FunctionLibrary/MyrmidonBlueprintFunctionLibrary.h"
 
+
+float UMyrmidonBlueprintFunctionLibrary::GetMinimalDistanceFromPointToRay(FVector Origin, FVector Forward,
+	FVector Target)
+{
+	FVector u = Forward.GetSafeNormal();
+	FVector v = (Target - Origin).GetSafeNormal();
+
+	float angle = FMath::Acos(FVector::DotProduct(u, v));
+	float distance = FVector::Dist(Origin, Target) * FMath::Sin(angle);
+
+	return distance;
+}
--- MyrmidonProto/Source/MyrmidonProto/Public/FunctionLibrary/MyrmidonBlueprintFunctionLibrary.h
+++ MyrmidonProto/Source/MyrmidonProto/Public/FunctionLibrary/MyrmidonBlueprintFunctionLibrary.h
@@ -13,5 +13,11 @@ UCLASS()
 class MYRMIDONPROTO_API UMyrmidonBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(Blueprintable, BlueprintPure, BlueprintCallable, Category = "Myrmidon")
+	static float GetMinimalDistanceFromPointToRay(FVector Origin, FVector Forward, FVector Target);
+
+	
 	
 };
