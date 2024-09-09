25245b727ce89d30376d1706178ff8c3f139127d
Arthur <daedras10@gmail.com>
2024-09-09 10:15:11 +0200

    New Aim with angle & dist

--- MyrmidonProto/Source/MyrmidonProto/Public/FunctionLibrary/MyrmidonBlueprintFunctionLibrary.h
+++ MyrmidonProto/Source/MyrmidonProto/Public/FunctionLibrary/MyrmidonBlueprintFunctionLibrary.h
@@ -6,6 +6,32 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "MyrmidonBlueprintFunctionLibrary.generated.h"
 
+USTRUCT(Blueprintable)
+struct FDistanceData
+{
+	GENERATED_BODY()
+
+public:
+	FDistanceData()
+	{
+		Distance = 0;
+		Angle = 0;
+	}
+	
+	FDistanceData(const float Distance, const float Angle)
+	{
+		this->Distance = Distance;
+		this->Angle = Angle;
+	}
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DistanceData")
+	float Distance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DistanceData")
+	float Angle;
+};
+
+
 /**
  * 
  */
@@ -16,7 +42,11 @@ class MYRMIDONPROTO_API UMyrmidonBlueprintFunctionLibrary : public UBlueprintFun
 
 public:
 	UFUNCTION(Blueprintable, BlueprintPure, BlueprintCallable, Category = "Myrmidon")
-	static float GetMinimalDistanceFromPointToRay(FVector Origin, FVector Forward, FVector Target);
+	static FDistanceData GetMinimalDistanceFromPointToRay(FVector Origin, FVector Forward, FVector Target);
+
+	
+	UFUNCTION(Blueprintable, BlueprintPure, BlueprintCallable, Category = "Myrmidon")
+	static bool IsDistanceDataValid(FDistanceData Data, float MaxDistance, float MaxAngle);
 
 	
 	
